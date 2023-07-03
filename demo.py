6b3c73143038a82f4e945804c3fdb3205a5b9848
Ruhijn <jnruhi334@gmail.com>
2023-07-03 09:00:42 +0530

    First Commit

---
+++ demo.py
@@ -0,0 +1,7 @@
+echo "# Data-Sem-5-" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Ruhijn/Data-Sem-5-.git
+git push -u origin main
